bab3591a58f6bf837aba4d6bc6f03d9cc438d211
Eric Lee <leric7135@gmail.com>
2023-05-04 09:58:00 -0700

    added cors and morgan packages, boiler plate set up of server and controllers

---
+++ server/controllers/index.js
@@ -0,0 +1,4 @@
+exports.details = require('./product_details.js');
+exports.q_a = require('./q_a.js');
+exports.related = require('./related_items.js');
+exports.reviews = require('./reviews.js');
--- server/index.js
+++ server/index.js
@@ -1,1 +1,36 @@
 //Router calls go here (fill in later)
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+
+//controllers
+const controllers = require('./controllers');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+//product details
+app.get('/', controllers)
+app.post('/', controllers)
+//product q_a
+app.get('/', controllers)
+app.post('/', controllers)
+//related
+app.get('/', controllers)
+app.post('/', controllers)
+//reviews
+app.get('/', controllers)
+app.post('/', controllers)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log(`Server listening at http://localhost:${PORT}`);
