ea4c71178a7009413b6b4027e29a1ca2cc513a21
Eric Lee <leric7135@gmail.com>
2023-05-07 17:53:29 -0700

    Product breakdown completed. need to style

--- client/src/components/reviews_ratings/components/ProductBreakdown.jsx
+++ client/src/components/reviews_ratings/components/ProductBreakdown.jsx
@@ -1,15 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import ReviewHelpers from './ReviewHelpers.jsx';
 
-const ProductBreakdown = () => {
-
+const ProductBreakdown = ({charObj}) => {
+  // const allCharacteristics = {
+  //   Size: { //135232
+  //     1: 'A size too small',
+  //     2: '1/2 a size too small',
+  //     3: 'Perfect',
+  //     4: '1/2 a size too big',
+  //     5: 'A size too wide',
+  //   },
+  //   Width: { //135233
+  //     1: 'Too narrow',
+  //     2: 'Slightly narrow',
+  //     3: 'Perfect',
+  //     4: 'Slightly Wide',
+  //     5: 'A size too wide',
+  //   },
+  //   Comfort: { //135221
+  //     1: 'Uncomfortable',
+  //     2: 'Slightly uncomfortable',
+  //     3: 'Ok',
+  //     4: 'Comfortable',
+  //     5: 'Perfect'
+  //   },
+  //   Quality: { //135222
+  //     1: 'Poor',
+  //     2: 'Below average',
+  //     3: 'What I expected',
+  //     4: 'Pretty great',
+  //     5: 'Perfect'
+  //   },
+  //   Length: { //135220
+  //     1: 'Runs short',
+  //     2: 'Runs slighty short',
+  //     3: 'Perfect',
+  //     4: 'Runs slightly long',
+  //     5: 'Runs long'
+  //   },
+  //   Fit: { //135219
+  //     1: 'Runs tight',
+  //     2: 'Runs slightly tight',
+  //     3: 'Perfect',
+  //     4: 'Runs slightly long',
+  //     5: 'Runs long'
+  //   }
+  // }
 //size, length. comfort, quality, width, fit
+
+//charObj = {id: ###, value: #.###}
+
+  const presentCharacteristics = {
+    135219: 'Fit',
+    135220: 'Length',
+    135221: 'Comfort',
+    135222: 'Quality',
+    135232: 'Size',
+    135233: 'Width'
+  };
+  const charPresent = presentCharacteristics[charObj.id]
+
+  let charTextLow
+  let charTextHigh
+
+  if(charPresent === 'Comfort' || charPresent === 'Quality') {
+    charTextLow = 'Poor'
+    charTextHigh= 'Perfect'
+
+  }
+// for each id match and display
+// let charTable = productRating.map((id, val, ind) => <ProductBreakdown id={productRating.id} val={productRating.value} key={ind}/>)
 
   return (
       <div>
-        <label>Size:</label>
-
+        <label>{charPresent}</label>
+        <br></br>
+        <meter value={charObj.value} max='5'></meter>
+        <br></br>
+        <span style={{fontSize:'0.4em'}}>{charTextLow}&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;{charTextHigh}</span>
       </div>
   )
 }
--- client/src/components/reviews_ratings/components/ReviewRating.jsx
+++ client/src/components/reviews_ratings/components/ReviewRating.jsx
@@ -1,17 +1,31 @@
 import React, {useState,useEffect} from 'react';
+import axios from 'axios';
 import ReviewList from './ReviewList.jsx';
 import testData from '../testdata/reviewdata.json';
 
 import SortBar from './SortBar.jsx';
 import AddReview from './AddReview/AddReview.jsx';
+import ProductBreakdown from './ProductBreakdown.jsx'
 
 const ReviewRating = () => {
   const [masterList, setMasterList] = useState([]);
   const [sortBar, setSortBar] = useState('relevant');
+  const [productRating, setProductRating] = useState([])
+
+  const getMeta = () => {
+    axios.get('/api/reviews/meta?product_id=40344')
+      .then(response => {
+        console.log(Object.values(response.data.characteristics))
+        setProductRating(Object.values(response.data.characteristics))
+      })
+  }
 
   useEffect(() => {
     setMasterList(testData.results)
+    getMeta()
   }, [])
+
+  let charTable = productRating.map((charObj, ind) => <ProductBreakdown charObj={charObj} key={ind}/>)
 
   //ratings filter needs to pass down without without influence on sorbar. should combo
   return (
@@ -23,7 +37,10 @@ const ReviewRating = () => {
           <section><h4>Rating Breakdown</h4></section>
         </div>
         <div>
-          <section><h4>Product Breakdown</h4></section>
+          <section>
+            <h4>Product Breakdown</h4>
+            {charTable}
+          </section>
         </div>
       </div>
 
