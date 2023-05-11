d065cab776bf009ef7968bd1b1f94cb2423d3667
Sara Li <ruimin.li77@gmail.com>
2023-05-10 18:37:46 -0700

    adjust css to show the center part of the main photo. set up image gallery structure

--- client/src/components/product_detail/ImageGallery.jsx
+++ client/src/components/product_detail/ImageGallery.jsx
@@ -1,18 +1,30 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function ImageGallery({ style }) {
-  // const
+function ImageGallery({ photos }) {
+  const [image, setImage] = useState(photos[0]);
+  const handleThumbnailClick = (e) => {
+    e.preventDefault();
+    const index = e.target.name;
+    setImage(photos[index]);
+  };
+
+  useEffect(() => {
+    setImage(photos[0]); // Update the currently displayed image when the photos prop changes
+  }, [photos]);
+
   return (
     <div className="image-gallery-div">
       <div className="image-gallery-thumbnails-div">
-        {style.photos.map((item) => <img src={item.thumbnail_url} className="image-gallery-thumbnail-img" key={item.url} />)}
+        {photos.map((item, index) => (
+          <div onClick={handleThumbnailClick} key={item.thumbnail_url}>
+            <img src={item.thumbnail_url} className="image-gallery-thumbnail-img" name={index} alt="thumbnail" />
+          </div>
+        ))}
       </div>
-      <img src={style.photos[0].url ? style.photos[0].url : 'https://shenandoahcountyva.us/bos/wp-content/uploads/sites/4/2018/01/picture-not-available-clipart-12.jpg'} className="main-photo-img" />
+      <img src={image.url ? image.url : 'https://shenandoahcountyva.us/bos/wp-content/uploads/sites/4/2018/01/picture-not-available-clipart-12.jpg'} className="main-photo-img" alt="mainphoto" />
     </div>
   );
 }
