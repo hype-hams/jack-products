9de7053256f9313e39c596b955596e7f52fa202f
cesargua <cguajard@ucsd.edu>
2023-05-11 11:25:10 -0700

    made outfit list data persist

--- client/src/components/related_items/OutfitListCard.jsx
+++ client/src/components/related_items/OutfitListCard.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 
-const OutfitListCard = ({card, currPhotoURL}) =>{
+const OutfitListCard = ({card, onDelete , handleRelatedItemClick}) =>{
+    const [photoURL, setPhotoURL] = useState('')
 
+    const GETPhotoURL = (id = card.id) =>{
+        const url = `http://localhost:3000/api/products`
+        axios.get(`${url}/${id}/styles`).then((res)=>{
+            const thumbnail_url = res.data.results[0].photos[0].thumbnail_url;
+            if(thumbnail_url !== null){
+                setPhotoURL(thumbnail_url);
+            } else {
+                setPhotoURL('images/image-not-found-icon.png');
+            }
+        }).catch(err=>{
+            console.error(err);
+        })
+    }
+    const onClickHandler = ()=>{
+        onDelete(card.id);
+    }
+    useEffect(()=>{
+        GETPhotoURL();
+    },[]);
     return (
-        <div>
-            <ul>
-                <img src={`${currPhotoURL}`}/>
-                <li>Category: {card.category}</li>
-                <li>Name: {card.name}</li>
-                <li>Price: {card.default_price}</li>
-                <li>Rating:{card.rating}</li>
-            </ul>
+        <div className='OutfitListCard'>
+            <button onClick={onClickHandler}>Delete</button>
+                <div onClick={()=>{handleRelatedItemClick(card.id)}}>    
+                    <img src={`${photoURL}`} /> 
+                    <p>{card.category}</p>
+                    <h2>{card.name}</h2>
+                    <p>${card.default_price}</p>
+                    <p>{card.rating}</p>
+                </div>
         </div>
     )
 
