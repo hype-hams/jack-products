660821b6ebfad25961736ad875b9a5178c6fb7f2
Sara Li <ruimin.li77@gmail.com>
2023-05-10 19:10:59 -0700

    image gallery partly functional, except scrolling the list and zooming the image.

--- client/src/components/product_detail/ImageGallery.jsx
+++ client/src/components/product_detail/ImageGallery.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
+import imageNotAvailable from './images/imageNotAvailable.png';
 
 function ImageGallery({ photos }) {
   const [image, setImage] = useState(photos[0]);
@@ -19,12 +20,12 @@ function ImageGallery({ photos }) {
     <div className="image-gallery-div">
       <div className="image-gallery-thumbnails-div">
         {photos.map((item, index) => (
-          <div onClick={handleThumbnailClick} key={item.thumbnail_url}>
-            <img src={item.thumbnail_url} className="image-gallery-thumbnail-img" name={index} alt="thumbnail" />
+          <div onClick={handleThumbnailClick} key={item.url}>
+            <img src={item.thumbnail_url || imageNotAvailable} name={index} className="image-gallery-thumbnail-img" alt="thumbnail" />
           </div>
         ))}
       </div>
-      <img src={image.url ? image.url : 'https://shenandoahcountyva.us/bos/wp-content/uploads/sites/4/2018/01/picture-not-available-clipart-12.jpg'} className="main-photo-img" alt="mainphoto" />
+      <img src={image.url || imageNotAvailable} className="main-photo-img" alt="mainphoto" />
     </div>
   );
 }
