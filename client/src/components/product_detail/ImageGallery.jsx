ce2e40b6a8379f8191be9fb0bc4553b5726aa6f8
Sara Li <ruimin.li77@gmail.com>
2023-05-11 02:30:12 -0700

    make highlight for selected thumbnail

--- client/src/components/product_detail/ImageGallery.jsx
+++ client/src/components/product_detail/ImageGallery.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
@@ -6,6 +7,7 @@ import imageNotAvailable from './images/imageNotAvailable.png';
 
 function ImageGallery({ photos }) {
   const [image, setImage] = useState(photos[0]);
+
   const handleThumbnailClick = (e) => {
     e.preventDefault();
     const index = e.target.name;
@@ -20,7 +22,7 @@ function ImageGallery({ photos }) {
     <div className="image-gallery-div">
       <div className="image-gallery-thumbnails-div">
         {photos.map((item, index) => (
-          <div onClick={handleThumbnailClick} key={item.url}>
+          <div style={item.url === image.url ? { borderBottom: 'thick solid white' } : { borderBottom: 'none' }} onClick={handleThumbnailClick} key={item.url}>
             <img src={item.thumbnail_url || imageNotAvailable} name={index} className="image-gallery-thumbnail-img" alt="thumbnail" />
           </div>
         ))}
@@ -31,3 +33,10 @@ function ImageGallery({ photos }) {
 }
 
 export default ImageGallery;
+
+/*
+
+    border-bottom-width: thick;
+    border-bottom-style: solid;
+    border-bottom-color: white;
+*/
