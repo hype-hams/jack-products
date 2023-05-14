98d8830bc848e8eeb642bb158c11311320f9ef59
Sara Li <ruimin.li77@gmail.com>
2023-05-14 14:47:41 -0700

    fully refactor the AddToCart component, using <div> and CSS instead of <select> tag to achieve the select-able function. When size is defalut, click on add-to-cart-btn will open the menu.

--- client/src/components/product_detail/AddToCart.jsx
+++ client/src/components/product_detail/AddToCart.jsx
@@ -1,73 +1,89 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const axios = require('axios');
 
 function AddToCart({ skus }) {
-  const [selectedSize, setSelectedSize] = useState('SELECT SIZE');
-  const [selectedQty, setSelectedQty] = useState('-');
-  const [sizeOptions, setSizeOptions] = useState(<option>SELECT SIZE</option>);
-  const [qtyOptions, setQtyOptions] = useState(<option>-</option>);
-  const [sizeDropdown, setSizeDropdown] = useState(false); // size dropdown default: able
-  const [qtyDropdown, setQtyDropdown] = useState(true); // quantity dropdown default: disabled
-  const [showButton, setShowButton] = useState(true);
+  const [size, setSize] = useState('SELECT SIZE');
+  const [quantity, setQuantity] = useState('-');
+  const [qtyOptions, setQtyOptions] = useState([]);
+  const [sizeMenuOpen, setSizeMenuOpen] = useState(false);
+  const [qtyMenuOpen, setQtyMenuOpen] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
+  const [disable, setDisable] = useState(false);
+  const sizeRef = useRef(null);
+  const qtyRef = useRef(null);
 
-  const selectRef = useRef(null);
-
-  useEffect(() => {
-    if (!skus[0].size) {
-      setSizeDropdown(true); // no stock, diable size dropdown
-      setSizeOptions(<option>OUT OF STOCK</option>);
-      setShowButton(false); // no stock, hide button
-    } else {
-      const sizeOptionsArr = skus.map((item) => (
-        <option key={item.sku_id} value={item.size}>{item.size}</option>));
-      setSizeOptions([sizeOptions, ...sizeOptionsArr]);
-    }
-  }, []);
-
-  // User changes size will affect Quantity Selector and Add-to-cart Button
-  const handleSizeChange = (e) => {
-    const currentSize = e.target.value;
-    setSelectedSize(currentSize); // Add to cart button needs selectedSize
-    // using currentSize instead of selectedSize because state updates are asynchronous
-    if (currentSize === 'SELECT SIZE') {
-      setQtyDropdown(true); // no size selected, disable quantity dropdown
-      setQtyOptions(<option>-</option>); // change quantity option to '-'
-    } else {
-      setQtyDropdown(false); // size selected, enable quantity dropdown
-      setSelectedQty(1); // Once a size has been selected, the quantity should default to 1.
-      const qtyForSelectedSize = skus.find((item) => item.size === currentSize).quantity;
-      // The maximum selection has a hard limit of 15
-      const limit = qtyForSelectedSize > 15 ? 15 : qtyForSelectedSize;
-      const qtyOptionsArr = [];
-      for (let i = 0; i < limit; i += 1) {
-        qtyOptionsArr.push(<option key={i + 1} value={i + 1}>{i + 1}</option>);
+  // close the dropdown menu anywhere on the page except itself and add-to-cart-btn
+  const handleClickOnPage = (event) => {
+    // if clicked item is button, we still want it open the size menu
+    const button = document.querySelector('.add-to-cart-btn');
+    if (button && !button.contains(event.target)) {
+      if (sizeRef.current && !sizeRef.current.contains(event.target)) {
+        setSizeMenuOpen(false);
       }
-      setQtyOptions(qtyOptionsArr);
+      if (qtyRef.current && !qtyRef.current.contains(event.target)) {
+        setQtyMenuOpen(false);
+      }
     }
   };
 
-  const handleQtyChange = (e) => {
-    setSelectedQty(e.target.value);
+  useEffect(() => {
+    // check null and set to out of stock
+    if (!skus[0].size) {
+      setSize('OUT OF STOCK');
+      setDisable(true);
+    } else {
+      setSize('SELECT SIZE');
+      setQuantity('-');
+      setDisable(false);
+      // handle close the menu when clicking on elsewhere
+      document.body.addEventListener('click', handleClickOnPage);
+    }
+    setSizeMenuOpen(false);
+    setQtyMenuOpen(false);
+  }, [skus]);
+
+  const handleQtyClick = (e) => {
+    setQuantity(Number(e.target.innerText));
+    setQtyMenuOpen(false);
+    setSizeMenuOpen(false);
+  };
+
+  const handleSizeClick = (e) => {
+    setQtyMenuOpen(false);
+    setSize(e.target.innerText);
+    if (e.target.innerText) {
+      setQuantity(1);
+      setSelectedId(e.target.getAttribute('data-sku_id'));
+      const totalQty = Number(e.target.getAttribute('data-quantity'));
+      const limit = totalQty > 15 ? 15 : totalQty;
+      // create an array with numbers from 1 to limit and set it to quantity
+      const qtyArr = [];
+      for (let i = 0; i < limit; i += 1) {
+        qtyArr.push(<section key={i + 1} onClick={handleQtyClick}>{i + 1}</section>);
+      }
+      setQtyOptions(qtyArr);
+    }
   };
 
   const handleAddToCartClick = (e) => {
     e.preventDefault();
-    if (selectedSize === 'SELECT SIZE') {
-      console.log('ref: ', selectRef.current);
-      selectRef.current.focus();
+    if (size === 'SELECT SIZE' || selectedId === null) {
+      setSizeMenuOpen(true);
     } else {
-      const id = skus.find((item) => item.size === selectedSize).sku_id;
-      console.log(`Adding { sku_id: ${id}, count: ${selectedQty} } to cart.....`);
-      // POST /cart only take sku_id as parameter, only add one count at a time.
-      // If there's more counts need to be added, have to post multiple requests
+      console.log(`Adding sku_id: ${selectedId}, size: ${size}, quantity: ${quantity} to cart...`);
       const multipleRequestsBasedOnCounts = async () => {
         const promises = [];
-        for (let i = 0; i < selectedQty; i += 1) {
-          promises.push(axios.post('/api/cart', { sku_id: id }));
+        for (let i = 0; i < quantity; i += 1) {
+          promises.push(axios.post('/api/cart', { sku_id: selectedId }));
         }
         const responses = await Promise.all(promises);
         responses.forEach((response, index) => {
@@ -78,24 +94,41 @@ function AddToCart({ skus }) {
     }
   };
 
+  const disabledStyle = { color: 'rgb(192, 192, 192)' };
+
   return (
     <div className="selectors">
-      <form>
-        <label>
-          <select id="size-selector" ref={selectRef} disabled={sizeDropdown} value={selectedSize} onChange={handleSizeChange} required>
-            {sizeOptions}
-          </select>
-        </label>
-        <label>
-          <select id="quantity-selector" disabled={qtyDropdown} value={selectedQty} onChange={handleQtyChange}>
-            {qtyOptions}
-          </select>
-        </label>
-        <br />
-        {showButton && <input type="submit" value="ADD TO BAG" id="addToCart-btn" onClick={handleAddToCartClick} />}
-      </form>
+      <div className="size-selector-div" ref={sizeRef} style={disable ? disabledStyle : null} onClick={() => (disable ? null : setSizeMenuOpen(!sizeMenuOpen))}>
+        {size}
+        {!disable && <div className="toggle-dropdown"><FontAwesomeIcon icon={faAngleDown} style={{ color: '#ababab' }} /></div>}
+      </div>
+
+      <div className="quantity-selector-div" ref={qtyRef} style={disable ? disabledStyle : null} onClick={() => (disable ? null : setQtyMenuOpen(!qtyMenuOpen))}>
+        {quantity}
+        {!disable && <div className="toggle-dropdown"><FontAwesomeIcon icon={faAngleDown} style={{ color: '#ababab' }} /></div>}
+      </div>
+
+      {sizeMenuOpen && (
+        <div className="size-dropdown-menu-div">
+          {skus.map((item) => (
+            <section
+              key={item.sku_id}
+              data-quantity={item.quantity}
+              data-sku_id={item.sku_id}
+              onClick={handleSizeClick}
+            >
+              {item.size}
+            </section>
+          ))}
+        </div>
+      )}
+      {qtyMenuOpen && <div className="qty-dropdown-menu-div">{qtyOptions}</div>}
+      <br />
+      <button className="add-to-cart-btn" disabled={disable} style={disable ? disabledStyle : null} onClick={handleAddToCartClick}>
+        ADD TO BAG
+        {!disable && <FontAwesomeIcon icon={faPlus} beat style={{ color: '#bababa' }} />}
+      </button>
     </div>
   );
 }
-
 export default AddToCart;
